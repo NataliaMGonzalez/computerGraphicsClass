655e984ad3070ff56be7d100dd6c3f51943d5044
NataliaMGonzalez <nataliagzzh@gmail.com>
2021-04-27 11:14:03 -0500

    Illumination intro

---
+++ 24-hello-lighting/js/main.js
@@ -0,0 +1,135 @@
+import * as THREE from "/build/three.module.js";
+import Stats from "/js/jsm/libs/stats.module.js";
+import {OrbitControls} from "/js/jsm/controls/OrbitControls.js";
+import * as dat from "/js/jsm/libs/dat.gui.module.js";
+
+"use strict";
+
+let renderer, scene, camera, box, cameraControls, gui, stats, pointlight;
+window.anim = false;
+
+function init(event) {
+    // RENDERER ENGINE
+    renderer = new THREE.WebGLRenderer({antialias: true});
+    renderer.setClearColor(new THREE.Color(0, 0, 0));
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    document.body.appendChild(renderer.domElement);
+
+    // SCENE
+    scene = new THREE.Scene();
+
+    // CAMERA
+    let fovy = 60.0;    // Field ov view
+    let aspectRatio = window.innerWidth / window.innerHeight;
+    let nearPlane = 0.1;
+    let farPlane = 10000.0;
+    camera = new THREE.PerspectiveCamera(fovy, aspectRatio, nearPlane, farPlane);
+    camera.position.set(2, 2, 5);
+    cameraControls = new OrbitControls(camera, renderer.domElement);
+
+    // LIGHT
+    // POINTLIGHT
+    var pointLightColor = "white";
+    var intensity = 1;
+    var distance = 0;
+    var decay = 1;
+    pointlight = new THREE.PointLight(pointLightColor, intensity, distance, decay);
+    pointlight.position.set(0, 2, 0);
+    var pointLightHelper = new THREE.PointLightHelper(pointlight, 0.1);
+            
+    // MODEL
+    // BOX
+    let geometry = new THREE.BoxGeometry(1);
+    let material = new THREE.MeshStandardMaterial({color: "red"});
+    box = new THREE.Mesh(geometry, material);
+    box.position.set(0, 0.5, 0);
+
+    // FLOOR
+    let floor = new Floor();
+
+    // SCENE HIERARCHY
+    scene.add(box);
+    scene.add(floor);
+    scene.add(pointlight);
+    scene.add(pointLightHelper);
+
+    // GUI
+    gui = new dat.GUI();
+    // SHOW/HIDE FLOOR
+    gui.add(floor, "visible").name("Floor").setValue(false).listen().onChange(function(value) {
+
+    });
+    gui.add(window, "anim").name("Animation").listen().onChange(function(value) {
+
+    });
+
+    gui.add(pointlight.position, "x").min(-3).max(3).step(0.1).setValue(0).name("Point Light X").listen().onChange(function(value){
+
+    });
+
+    let params = {
+        boxColor: new THREE.Color(1,1,1),
+        pointLightColor: new THREE.Color(1,1,1)
+    }
+
+    gui.addColor(params, "boxColor").name("Box Color").listen().onChange(function(color){
+        box.material.color = new THREE.Color(color.r/255, color.g/255, color.b/255);
+    }); 
+
+    gui.addColor(params, "pointLightColor").name("Point Light Color").listen().onChange(function(color){
+        pointlight.color = new THREE.Color(color.r/255, color.g/255, color.b/255);
+    });
+
+    gui.open();
+
+    // SETUP STATS
+    stats = new Stats();
+    stats.showPanel(0); // 0: fps, 1: ms, 2: mb, 3+: custom
+    document.body.appendChild(stats.dom);
+
+    // DRAW SCENE IN A RENDER LOOP (ANIMATION)
+    renderLoop();
+}
+
+function renderLoop() {
+    stats.begin();
+    renderer.render(scene, camera); // DRAW SCENE
+    updateScene();
+    stats.end();
+    stats.update();
+    requestAnimationFrame(renderLoop);
+}
+
+function updateScene() {
+    if(anim) {
+        box.rotation.x = box.rotation.x + 0.01;
+        box.rotation.y = box.rotation.y + 0.01;
+    }
+}
+
+// EVENT LISTENERS & HANDLERS
+
+document.addEventListener("DOMContentLoaded", init);
+
+window.addEventListener("resize", () => {
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+    cameraControls.update();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+}, false);
+
+// MODELS
+class Floor extends THREE.Mesh {
+    constructor() {
+        super();
+        this.geometry = new THREE.PlaneGeometry(10, 10, 10, 10);
+        this.material = new THREE.MeshBasicMaterial();
+        this.rotation.x = -0.5 * Math.PI;
+        this.wireframeHelper = new THREE.LineSegments(new THREE.WireframeGeometry(this.geometry));
+        this.wireframeHelper.material.color = new THREE.Color(0.2, 0.2, 0.2);
+        this.add(this.wireframeHelper);
+        this.visible = false;
+    }
+}
+
+
